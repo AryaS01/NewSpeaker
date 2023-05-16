4f96b106cf290fcb18d1b5483dbbfe9421a4f3fc
Arya <Aryaa.swarnappan@gmail.com>
2023-05-16 14:49:41 +0530

    Set news title and description

--- src/Components/News.js
+++ src/Components/News.js
@@ -4,15 +4,21 @@ import Newsitem from './Newsitem'
 export class News extends Component {
   render() {
     return (
-      <div>
-This is the news component
-<Newsitem/>
-<Newsitem/>
-<Newsitem/>
-<Newsitem/>
-<Newsitem/>
-<Newsitem/>
+      <div className="container my-3">
+<h1>NewSpeaker- Top headlines</h1>
+<div className="row">
+    <div className="col-md-3">
+<Newsitem title="Arya" description="Hey there!"/>
+</div>
 
+<div className="col-md-3">
+<Newsitem title="Arya" description="Hey there!"/>
+</div>
+
+<div className="col-md-3">
+<Newsitem title="Arya" description="Hey there!"/>
+</div>
+      </div>
       </div>
     )
   }
