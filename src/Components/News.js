10df983e21146dd3b2c1041b25513d567c89c633
Arya <Aryaa.swarnappan@gmail.com>
2023-05-18 13:15:46 +0530

    Fetch news

--- src/Components/News.js
+++ src/Components/News.js
@@ -2,22 +2,32 @@ import React, { Component } from 'react'
 import Newsitem from './Newsitem'
 
 export class News extends Component {
+  
+  constructor(){
+    super();
+    console.log("Im the constructor of news component")
+    this.state={
+      articles:[],
+      loading:false
+    }
+  }
+async componentDidMount(){
+ let url="https://newsapi.org/v2/top-headlines?country=us&apiKey=849f21d206d740cba44653968bc25396";
+ let data = await fetch(url);
+let parsedData = await data.json();
+ console.log(parsedData);
+ this.setState({articles: parsedData.articles})
+};
   render() {
     return (
       <div className="container my-3">
-<h1>NewSpeaker- Top headlines</h1>
+<h2>NewSpeaker- Top headlines</h2>
 <div className="row">
-    <div className="col-md-3">
-<Newsitem title="Arya" description="Hey there!"/>
+  {this.state.articles.map((element)=>{
+    return <div className="col-md-3" key={element.url}>
+<Newsitem  title={element.title?element.title.slice(0,45):""} description={element.description?element.description.slice(0,88):""} imageUrl={element.urlToImage} newsUrl={element.url}/>
 </div>
-
-<div className="col-md-3">
-<Newsitem title="Arya" description="Hey there!"/>
-</div>
-
-<div className="col-md-3">
-<Newsitem title="Arya" description="Hey there!"/>
-</div>
+  })}
       </div>
       </div>
     )
--- src/Components/Newsitem.js
+++ src/Components/Newsitem.js
@@ -2,18 +2,19 @@ import React, { Component } from "react";
 
 export class Newsitem extends Component {
   render() {
-    let {title,description}=this.props
+    let {title,description,imageUrl,newsUrl}=this.props
     return (
       <div>
         <div className="card" style={{width:"18rem"}}>
-          <img src="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg" className="card-img-top" alt="..." />
+        <img src={imageUrl ? imageUrl : "https://img.freepik.com/premium-vector/default-image-icon-vector-missing-picture-page-website-design-mobile-app-no-photo-available_87543-7509.jpg?w=900"} className="card-img-top" alt="..." />
+
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">
-             {description}
+             {description}...
             </p>
-            <a href="/" className="btn btn-primary">
-              Go somewhere
+            <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-primary">
+              Read more
             </a>
           </div>
         </div>
